767d704ab23c1de0372e2348732b6c3dde497e68
milanariga <anastasia.i2811@gmail.com>
2021-02-13 21:33:14 +0200

    Project initialization

---
+++ src/main/java/com/ana/PrivateInsurancePortfolio/PrivateInsurancePortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.ana.PrivateInsurancePortfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateInsurancePortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateInsurancePortfolioApplication.class, args);
+	}
+
+}
