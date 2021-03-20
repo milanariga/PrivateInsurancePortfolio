5bbdb553ca5c4f572b7f6195d08ca1a4e3531401
milanariga <anastasia.i2811@gmail.com>
2021-03-20 18:12:32 +0200

    Register systemUser added

--- src/main/java/com/ana/PrivateInsurancePortfolio/controller/UserController.java
+++ src/main/java/com/ana/PrivateInsurancePortfolio/controller/UserController.java
@@ -3,9 +3,15 @@ package com.ana.PrivateInsurancePortfolio.controller;
 import com.ana.PrivateInsurancePortfolio.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
     private final UserRepository userRepository;
@@ -14,10 +20,21 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
-    @RequestMapping("/users")
-    public String getUsers(Model model){
-        model.addAttribute("users", userRepository.findAll());
+    @PostMapping("/register")
+    public String registerSystemUser(@RequestBody Map<String, Object> systemUserMap){
+        String firstName = (String) systemUserMap.get("firstName");
+        String lastName = (String) systemUserMap.get("lastName");
+        String login = (String) systemUserMap.get("login");
+        String password = (String) systemUserMap.get("password");
+        String email = (String) systemUserMap.get("email");
 
-        return "users/list";
+        return firstName + " " + lastName + ", " + login + " " + password + ", " + email;
     }
+
+//    @RequestMapping("/users")
+//    public String getUsers(Model model){
+//        model.addAttribute("users", userRepository.findAll());
+//
+//        return "users/list";
+//    }
 }
